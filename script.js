62135aac7c708d0a95abeab73e1de973ff54615a
Raymond Zeng <shilei.zeng98@outlook.com>
2023-10-07 15:08:00 -0400

    Added setFolderActive function

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-import {getColorFilter} from "./color";
 
 
-/*Feature: display options of rocket part folder selectd*/
-const folders = document.querySelectorAll(".folder");
-console.log(folders)
+(function(){/*Feature: display options of rocket part folder selectd*/
+  const folders = document.querySelectorAll(".folder");
+  folders.forEach((folder) => {
+    folder.addEventListener("click", function(e){
+      setFolderActive(e)
+    })
+  })
+}());
+
+
+function setFolderActive(e){
+  console.log("Hello");
+}
